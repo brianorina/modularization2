95f7689f3ba8821eabf96da151a26cb8719fe65a
brianorina <orinabrian100@gmail.com>
2016-05-10 20:10:18 +0300

    Add files via upload

---
+++ shape.cpp
@@ -0,0 +1,16 @@
+//Guard the header class
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include <string>
+
+ class Shape{
+ protected:
+ std::string name;
+ public:
+ Shape(std::string name = "Amorphous Base Shape"): name(name){}
+ 
+ std::string getName();
+};
+ 
+#endif
